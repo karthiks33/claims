2f5270c293b3ef40516d6c5052639ce6817ab2ad
revjaya <revjaya161@gmail.com>
2019-02-27 20:49:07 -0800

    added urls

--- src/main/java/com/claims/ClaimsApplication.java
+++ src/main/java/com/claims/ClaimsApplication.java
@@ -2,20 +2,22 @@ package com.claims;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, SecurityAutoConfiguration.class})
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@EnableJpaRepositories("com.claims.repository")
 public class ClaimsApplication extends SpringBootServletInitializer {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(ClaimsApplication.class);
-	}
 
-	public static void main(String[] args) {
-		SpringApplication.run(ClaimsApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ClaimsApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClaimsApplication.class, args);
+    }
 }
--- src/main/java/com/claims/controller/ClaimsController.java
+++ src/main/java/com/claims/controller/ClaimsController.java
@@ -1,30 +1,29 @@
 package com.claims.controller;
 
-import java.net.URI;
-import java.util.List;
-
-import com.claims.model.Course;
+import com.claims.model.Information;
+import com.claims.model.Result;
+import com.claims.repository.InformationRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.claims.service.StudentService;
-
 import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
 
 @RestController
 public class ClaimsController {
 
+    private static final Logger logger = LoggerFactory.getLogger(ClaimsController.class);
+
     @Autowired
-    private StudentService studentService;
+    private InformationRepository informationRepository;
 
     @GetMapping("/")
-    public void welcomePage(HttpServletResponse httpServletResponse){
+    public void welcomePage(HttpServletResponse httpServletResponse) {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path(
                 "/test.html").buildAndExpand().toUri();
         httpServletResponse.setHeader("Location", uri.toString());
@@ -36,16 +35,70 @@ public class ClaimsController {
         return "success";
     }
 
-    @GetMapping("/students/{studentId}/courses")
-    public List<Course> retrieveCoursesForStudent(@PathVariable String studentId) {
-        return studentService.retrieveCourses(studentId);
+    @RequestMapping(value = "/info/save", method = RequestMethod.POST, produces = "application/json")
+    public ResponseEntity<?> saveInfo(@RequestBody Information info) {
+        try {
+            logger.info("Saving info");
+            informationRepository.save(info);
+            return new ResponseEntity<>("Saved info", HttpStatus.OK);
+        } catch (Exception ex) {
+            String errorMessage;
+            errorMessage = ex + " <== error";
+            return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+        }
     }
 
-    @PostMapping("/students/{studentId}/courses")
+    @RequestMapping(value = "/info/{id}", method = RequestMethod.GET, produces = "application/json")
+    public ResponseEntity<?> getInfo(@PathVariable Long id) {
+        try {
+            logger.info("Getting info {}", id);
+            return new ResponseEntity<>(informationRepository.findById(id), HttpStatus.OK);
+        } catch (Exception ex) {
+            String errorMessage;
+            errorMessage = ex + " <== error";
+            return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @RequestMapping(value = "/info/all", method = RequestMethod.GET, produces = "application/json")
+    public ResponseEntity<?> getAllInfo() {
+        try {
+            logger.info("Getting all info");
+            return new ResponseEntity<>(informationRepository.findAll(), HttpStatus.OK);
+        } catch (Exception ex) {
+            String errorMessage;
+            errorMessage = ex + " <== error";
+            return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = "application/json")
+    public ResponseEntity<?> getClaimsInfo(@PathVariable String id) {
+        try {
+            logger.info("Getting claims info {}", id);
+            long infoId;
+            if (id.equals("dashboard")) {
+                infoId = 1;
+            } else if (id.equals("incidents")) {
+                infoId = 2;
+            } else if (id.equals("customers")) {
+                infoId = 3;
+            } else if (id.equals("about")) {
+                infoId = 4;
+            } else {
+                throw new Exception("Invalid info");
+            }
+            return new ResponseEntity<>(informationRepository.findById(infoId), HttpStatus.OK);
+        } catch (Exception ex) {
+            String errorMessage = ex + " <== error";
+            return new ResponseEntity<>(new Result(errorMessage), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    //Reference
+/*    @PostMapping("/students/{studentId}/courses")
     public ResponseEntity<Void> registerStudentForCourse(
             @PathVariable String studentId, @RequestBody Course newCourse) {
-
-        Course course = studentService.addCourse(studentId, newCourse);
 
         if (course == null)
             return ResponseEntity.noContent().build();
@@ -54,12 +107,7 @@ public class ClaimsController {
                 "/{id}").buildAndExpand(course.getId()).toUri();
 
         return ResponseEntity.created(location).build();
-    }
+    }*/
 
-    @GetMapping("/students/{studentId}/courses/{courseId}")
-    public Course retrieveDetailsForCourse(@PathVariable String studentId,
-                                           @PathVariable String courseId) {
-        return studentService.retrieveCourse(studentId, courseId);
-    }
 
 }
